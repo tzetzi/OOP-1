6e7002bf88d3c9a80eef46b65427973a2a5e57b2
Галя Георгиева <alice.gospodinova@gmail.com>
2015-06-07 16:32:46 +0300

    commit 02Exceptions

---
+++ 02Exceptions/01SquareRoot/SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class SquareRoot
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        try
+        {
+            int num = int.Parse(str);
+            if (num < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Console.WriteLine("You entered a valid Int32 number {0}.", str);
+            double result = Math.Sqrt(num);
+            Console.WriteLine("Square root:"+result);
+
+        }
+        catch (FormatException fe)
+        {
+
+            Console.Error.WriteLine("Invalid format of integer number!\n" + fe);
+        }
+        catch(ArgumentOutOfRangeException)
+        {
+            Console.Error.WriteLine("Integer number cannot take negative value {0}!");
+        }
+        catch (OverflowException)
+        {
+            Console.Error.WriteLine("The number is too big for Int32!");
+
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ 02Exceptions/02EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/*
+ * Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end]. If an invalid number or non-number text is entered, the method should throw an exception. Using this method write a program that enters 10 numbers: a1, a2, … a10, such that 1 < a1 < … < a10 < 100. If the user enters an invalid number, let the user to enter again.
+ */
+namespace _02EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int counter = 1;
+            int start = 1;
+            string input = Console.ReadLine();
+            while (counter<=10)
+	        {
+                try
+                {
+                    int n = int.Parse(input);
+                    ReadNumber(start,n);
+                    counter++;
+                    start = n;
+                }
+                catch (FormatException fe)
+                {
+                    Console.Error.WriteLine("{0}", fe.Message);
+
+                }
+                catch (ArgumentOutOfRangeException outRange)
+                {
+                    Console.Error.WriteLine("The input number has to be in range [{0}..100]\n {1}",start, outRange.ParamName);
+                }
+                finally
+                {
+                    Console.WriteLine("Enter new number in  range [{0}..100]:",start);
+                    input = Console.ReadLine();
+                }
+            }
+        }
+
+        private static void ReadNumber(int start, int n)
+        {
+            if (n<=start || n>=100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
