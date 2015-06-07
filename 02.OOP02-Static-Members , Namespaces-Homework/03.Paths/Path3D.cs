c3fa150468734808bdce38e82f5c3d71a76c8573
Галя Георгиева <alice.gospodinova@gmail.com>
2015-06-07 13:18:25 +0300

    COMMIT Problem 3. Paths

---
+++ 02.OOP02-Static-Members , Namespaces-Homework/03.Paths/Path3D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using _01.Point3D;
+
+namespace _03.Paths
+{
+    class Path3D
+    {
+        private readonly List<Point3D> path = new List<Point3D>();
+
+        //ctor
+        public Path3D(List<Point3D> list)
+        {
+            this.path=list;
+        }
+               
+        //prop
+        public List<Point3D> Path
+        {
+            get { return this.path; }
+        }
+
+        //method
+        public void AddPoints(params Point3D[] list)
+        {
+            foreach (var point3D in list)
+            {
+                this.path.Add(point3D);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder("Path:\n");
+            for (int i = 0; i < this.Path.Count; i++)
+            {
+                result.AppendFormat("point{0}: x={1} y={2} z={3}\n", i + 1, this.Path[i].X, this.Path[i].Y, this.Path[i].Z);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ 02.OOP02-Static-Members , Namespaces-Homework/03.Paths/Paths3DTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _01.Point3D;
+using System.Collections.Generic;
+
+namespace _03.Paths
+{
+    class Paths3DTest
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Point3D point1 = new Point3D(0, 5, 1);
+                Point3D point2 = new Point3D(-3, 20, 0);
+                Path3D path = new Path3D(new List<Point3D>() { point2, point1 });
+
+                Storage.SavePathToFile("../../path.txt", path.ToString());
+                Console.WriteLine("Load from file:\n" + Storage.LoadPathFromFile("../../path.txt"));
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
